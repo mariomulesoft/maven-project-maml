184c9cb4906f09b5e0d781bca7191a69fc66bc61
Mario Martinez <mmartinez@Marios-MacBook-Pro.local>
2017-03-13 13:38:25 +0100

    Added header validation test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,6 +8,8 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
